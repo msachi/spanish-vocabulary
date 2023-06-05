9c2467c4d25a7e779b39ccabf6cf3ee5de5fd472
marko.sustarsic@argos.co.uk <marchee88@gmail.com>
2023-06-05 13:12:07 +0100

    Fix bug

--- api/queries/getRandomWord.js
+++ api/queries/getRandomWord.js
@@ -2,7 +2,7 @@ const dbConn = require('../database/db_connection');
 
 module.exports = async (language, category) => {
   try {
-    const data = await dbConn.query(`SELECT * FROM words WHERE catagory=($1) ORDER BY RANDOM() LIMIT 1`, [category]);
+    const data = await dbConn.query(`SELECT * FROM words WHERE category=($1) ORDER BY RANDOM() LIMIT 1`, [category]);
     return data.rows[0][language];
   } catch (error) {
     console.error(error);
